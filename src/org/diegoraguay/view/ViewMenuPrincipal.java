f78ad64bfd1916298e62c67a094f7ae74965f5a0
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-04-12 19:24:56 -0600

    006 Menu operaciones logicas hecho

--- src/org/diegoraguay/view/ViewMenuPrincipal.java
+++ src/org/diegoraguay/view/ViewMenuPrincipal.java
@@ -10,7 +10,7 @@ public class ViewMenuPrincipal {
             System.out.println("|______Menu Principal______|");
             System.out.println("|1.Operaciones matematicas |");
             System.out.println("|2.Operaciones de texto    |");
-            System.out.println("|3.Operaciones de logica   |");
+            System.out.println("|3.Operaciones logicas     |");
             System.out.println("|4.Salir del menu          |");
             System.out.println("|__________________________|");
             System.out.println("Seleccione una opcion");
@@ -24,7 +24,7 @@ public class ViewMenuPrincipal {
                     mostrarMenuOperacionesDeTexto();
                     break;
                 case 3:
-                    System.out.println("Entrando a menu Operaciones de logica");
+                    mostrarMenuOperacionesLogicas();
                     break;
                 case 4:
                     System.out.println("Saliendo del menu");
@@ -161,4 +161,65 @@ public void mostrarMenuOperacionesDeTexto(){
         
     }while (opcionTexto !=11); 
   }
+
+ public void mostrarMenuOperacionesLogicas(){
+    int opcionLogica = 0; 
+     
+    do{
+        System.out.println("|_Menu operaciones logicas_|");
+        System.out.println("|1.Verificador de edad     |");
+        System.out.println("|2.Numero par              |");
+        System.out.println("|3.Semaforo                |");
+        System.out.println("|4.Rango numerico          |");
+        System.out.println("|5.Login simple            |");
+        System.out.println("|6.Mayor de dos            |");
+        System.out.println("|7.Año bisiesto            |");
+        System.out.println("|8.Aprobado/reprobado      |");
+        System.out.println("|9.Validador de triangulo  |");
+        System.out.println("|10.Comparador de cadenas  |");
+        System.out.println("|11.Salir del menu         |");
+        System.out.println("|__________________________|");
+        System.out.println("Seleccione una opcion");
+        
+        opcionLogica = Integer.parseInt(leer.nextLine());
+        
+        switch(opcionLogica){
+            case 1: 
+                System.out.println("Verificador de edad");
+                break; 
+            case 2:
+                System.out.println("Numero par");
+                break; 
+            case 3:
+                System.out.println("Semaforo");
+                break; 
+            case 4:    
+                System.out.println("Rango numerico");
+                break; 
+            case 5:   
+                System.out.println("Login simple");
+                break; 
+            case 6:
+                System.out.println("Mayor de dos");
+                break; 
+            case 7:
+                System.out.println("Año bisiesto ");
+                break; 
+            case 8:
+                System.out.println("Aprobado/reprobado");
+                break; 
+            case 9:
+                System.out.println("Validador de triangulo");
+                break; 
+            case 10:
+                System.out.println("Comparador de cadenas");
+                break; 
+            case 11:
+                System.out.println("Saliendo del Menu operaciones logicas");
+                break;
+            default :
+                System.out.println("Opcion invalida, mantener el rango(1-11)");
+        }
+    }while(opcionLogica !=11);
+ }
 }
